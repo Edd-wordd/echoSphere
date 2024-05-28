e50568a8a10737720a994af17fcbccfa7ee9606a
eddwordd <edward.plasencio@gmail.com>
2024-05-27 21:33:08 -0500

    refactor tests, add sing up page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignUp from './components/pages/SignUp'
-import Login from './components/pages/Login'
+import SignIn from './components/pages/SignIn'
 import Dashboard from './components/pages/Dashboard'
 import Notfound from './components/pages/Notfound'
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<Notfound />} />
-        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   )
--- src/tests/components/pages/AppRoutes.test.js
+++ src/tests/components/pages/AppRoutes.test.js
@@ -1,30 +1,21 @@
-// import React from 'react'
-// import { render, screen } from '@testing-library/react'
-// import App from '../../../App'
-//
-// describe('Route Tests', () => {
-//   test('renders Login page when navigating to / route', async () => {
-//     window.history.pushState({}, '', '/')
-//
-//     render(<App />)
-//
-//     const loginPageText = await screen.findByText(/Login Page/i)
-//     expect(loginPageText).toBeInTheDocument()
-//   })
-// })
-
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from '../../../App'
 
 describe('Route Tests', () => {
-  test('renders Login page when navigating to / route', async () => {
+  test('renders SignIn page when navigating to / route', async () => {
     window.history.pushState({}, '', '/')
 
     render(<App />)
 
-    const loginPageText = await screen.findByText(/Login Page/i)
-    expect(loginPageText).toBeInTheDocument()
+    const loginHeading = await screen.findByRole('heading', { name: /Sign in/i })
+    expect(loginHeading).toBeInTheDocument()
+
+    const emailField = await screen.findByLabelText(/Email Address/i)
+    expect(emailField).toBeInTheDocument()
+
+    const passwordField = await screen.findByLabelText(/Password/i)
+    expect(passwordField).toBeInTheDocument()
   })
 
   test('renders Dashboard page when navigating to /dashboard route', async () => {
