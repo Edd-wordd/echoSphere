9c4ebea4a41f400b894ed1edbec56edc8c85724c
eddwordd <edward.plasencio@gmail.com>
2024-06-19 22:48:33 -0500

    sign in user

--- src/components/pages/SignIn.jsx
+++ src/components/pages/SignIn.jsx
@@ -12,18 +12,95 @@ import Grid from '@mui/material/Grid'
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
 import Typography from '@mui/material/Typography'
 import Footer from '../../components/pages/Footer'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore'
-import { firestore } from '../../firebase/firebase'
+import { auth, firestore } from '../../firebase/firebase'
 import GoogleIcon from '@mui/icons-material/Google'
 import GitHubIcon from '@mui/icons-material/GitHub'
-import FacebookIcon from '@mui/icons-material/Facebook' // Adjust the path as necessary
+import FacebookIcon from '@mui/icons-material/Facebook'
+import Alert from '@mui/material/Alert'
+import {
+  FacebookAuthProvider,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithRedirect,
+} from 'firebase/auth'
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress' // Adjust the path as necessary
+import { useNavigate } from 'react-router-dom' // or your preferred routing library
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export default function SignIn() {
+  const navigate = useNavigate() // useNavigate hook returns a navigate function
+
   const [userCredentials, setUserCredentials] = useState({
     email: '',
     password: '',
   })
+  const [errorMessage, setErrorMessage] = useState('')
+  const [isSocialMediaSigningIn, setIsSocialMediaSigningIn] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleGoogleSignIn = async (e) => {
+    e.preventDefault()
+    setIsSocialMediaSigningIn(true)
+    const provider = new GoogleAuthProvider()
+    try {
+      await signInWithRedirect(auth, provider)
+    } catch (error) {
+      console.error('Error during sign-in with redirect:', error)
+      setErrorMessage('Error during sign-in. Please try again.')
+      setIsSocialMediaSigningIn(false)
+    }
+  }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, redirect to dashboard
+        navigate('/dashboard')
+      }
+    })
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe()
+  }, [navigate])
+  const handleGitHubSignIn = async (e) => {
+    e.preventDefault()
+    setIsSocialMediaSigningIn(true)
+    const provider = new GithubAuthProvider()
+    try {
+      await signInWithRedirect(auth, provider)
+    } catch (error) {
+      console.error('Error during sign-in with redirect:', error)
+      setErrorMessage('Error during sign-in. Please try again.')
+      setIsSocialMediaSigningIn(false)
+    }
+  }
+
+  const handleFacebookSignIn = async (e) => {
+    e.preventDefault()
+    setIsSocialMediaSigningIn(true)
+    const provider = new FacebookAuthProvider()
+    try {
+      await signInWithRedirect(auth, provider)
+    } catch (error) {
+      console.error('Error during sign-in with redirect:', error)
+      setErrorMessage('Error during sign-in. Please try again.')
+      setIsSocialMediaSigningIn(false)
+    }
+  }
+
+  useEffect(() => {
+    let timer
+    if (errorMessage) {
+      timer = setTimeout(() => {
+        setErrorMessage('')
+      }, 3000)
+    }
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [errorMessage])
 
   const handleChange = (e) => {
     const { value, name } = e.target
@@ -32,6 +109,27 @@ export default function SignIn() {
 
   const handleSubmit = async (event) => {
     event.preventDefault()
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+
+    if (userCredentials.email === '' || userCredentials.password === '') {
+      console.log('Email or password is empty')
+      setErrorMessage('Email or password is empty')
+      setIsSubmitting(false)
+
+      return
+    }
+
+    if (!emailRegex.test(userCredentials.email)) {
+      console.log('Invalid email address')
+      setErrorMessage('Invalid email address')
+      setIsSubmitting(false)
+
+      return
+    }
+
+    setIsSubmitting(true)
+
     try {
       // Log the user credentials for debugging purposes
       console.log('User credentials:', userCredentials)
@@ -50,6 +148,7 @@ export default function SignIn() {
       } else {
         // Log that no documents were found
         console.log('No such user!')
+        setErrorMessage('No such user!')
       }
     } catch (error) {
       // Log any errors that occur during the retrieval
@@ -60,6 +159,12 @@ export default function SignIn() {
   return (
     <Grid container component="main" sx={{ height: '100vh' }}>
       <CssBaseline />
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isSubmitting || isSocialMediaSigningIn}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <Grid
         item
         xs={false}
@@ -101,6 +206,7 @@ export default function SignIn() {
             Sign in
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
             <TextField
               margin="normal"
               required
@@ -154,7 +260,7 @@ export default function SignIn() {
                 fullWidth
                 variant="outlined"
                 startIcon={<GoogleIcon />}
-                // onClick={handleGoogleSignIn}
+                onClick={handleGoogleSignIn}
               >
                 Sign in with Google
               </Button>
@@ -162,7 +268,7 @@ export default function SignIn() {
                 fullWidth
                 variant="outlined"
                 startIcon={<GitHubIcon />}
-                // onClick={handleGitHubSignIn}
+                onClick={handleGitHubSignIn}
               >
                 Sign in with GitHub
               </Button>
@@ -170,7 +276,7 @@ export default function SignIn() {
                 fullWidth
                 variant="outlined"
                 startIcon={<FacebookIcon />}
-                // onClick={handleFacebookSignIn}
+                onClick={handleFacebookSignIn}
               >
                 Sign in with Facebook
               </Button>
