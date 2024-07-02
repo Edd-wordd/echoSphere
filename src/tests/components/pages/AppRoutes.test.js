f3d95f60538e976a1373571d1c8c9f6d2fbe0aa3
eddwordd <edward.plasencio@gmail.com>
2024-07-02 00:41:28 -0500

    test edit

--- src/tests/components/pages/AppRoutes.test.js
+++ src/tests/components/pages/AppRoutes.test.js
@@ -1,69 +1,69 @@
-// import React from 'react'
-// import { render, screen, waitFor } from '@testing-library/react'
-// import App from '../../../App'
-// import { getAuth } from 'firebase/auth'
-//
-// // Mock firebase/auth module
-// jest.mock('firebase/auth', () => {
-//   const originalModule = jest.requireActual('firebase/auth')
-//   return {
-//     ...originalModule,
-//     getAuth: jest.fn(() => ({
-//       onAuthStateChanged: jest.fn(),
-//       signOut: jest.fn(),
-//     })),
-//   }
-// })
-//
-// describe('Route Tests', () => {
-//   let authMock
-//
-//   beforeAll(() => {
-//     authMock = getAuth()
-//     authMock.onAuthStateChanged.mockImplementation((callback) => {
-//       callback({ displayName: 'Test User' }) // Mock a logged-in user
-//       return jest.fn() // Mock the unsubscribe function
-//     })
-//   })
-//
-//   test('renders SignIn page when navigating to / route', async () => {
-//     window.history.pushState({}, '', '/')
-//
-//     render(<App />)
-//
-//     const loginHeading = await screen.findByRole('heading', { name: /Sign in/i })
-//     expect(loginHeading).toBeInTheDocument()
-//
-//     const emailField = await screen.findByLabelText(/Email Address/i)
-//     expect(emailField).toBeInTheDocument()
-//
-//     const passwordField = await screen.findByLabelText(/Password/i)
-//     expect(passwordField).toBeInTheDocument()
-//   })
-//
-//   test('renders Dashboard page when navigating to /dashboard route', async () => {
-//     window.history.pushState({}, '', '/dashboard')
-//
-//     render(<App />)
-//
-//     await waitFor(() => {
-//       const dashboardPageText = screen.getByText(/Dashboard/i)
-//       expect(dashboardPageText).toBeInTheDocument()
-//     })
-//   })
-//
-//   test('renders Profile page when navigating to /signup', async () => {
-//     window.history.pushState({}, '', '/signup')
-//
-//     render(<App />)
-//
-//     const loginHeading = await screen.findByRole('heading', { name: /Sign up/i })
-//     expect(loginHeading).toBeInTheDocument()
-//
-//     const emailField = await screen.findByLabelText(/Email Address/i)
-//     expect(emailField).toBeInTheDocument()
-//
-//     const passwordField = await screen.findByLabelText(/Password/i)
-//     expect(passwordField).toBeInTheDocument()
-//   })
-// })
+import React from 'react'
+import { render, screen, waitFor } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from '../../../App'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+// Mock Firebase Auth
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn(),
+  onAuthStateChanged: jest.fn(),
+  signInWithPopup: jest.fn(),
+  signInWithEmailAndPassword: jest.fn(),
+  signOut: jest.fn(),
+}))
+
+describe('Route Tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('renders SignIn page when navigating to / route', async () => {
+    window.history.pushState({}, '', '/')
+
+    render(
+      <Router>
+        <App />
+      </Router>,
+    )
+
+    await waitFor(() => {
+      expect(screen.getByText(/Sign in/i)).toBeInTheDocument()
+    })
+  })
+
+  test('renders Dashboard page when navigating to /dashboard route', async () => {
+    const mockUser = { email: 'test@example.com', emailVerified: true }
+    const mockOnAuthStateChanged = (auth, callback) => {
+      callback(mockUser)
+      return jest.fn() // Mock the unsubscribe function
+    }
+    onAuthStateChanged.mockImplementation(mockOnAuthStateChanged)
+
+    window.history.pushState({}, '', '/dashboard')
+
+    render(
+      <Router>
+        <App />
+      </Router>,
+    )
+
+    await waitFor(() => {
+      expect(screen.getByText(/Dashboard/i)).toBeInTheDocument()
+    })
+  })
+
+  test('renders Profile page when navigating to /signup route', async () => {
+    window.history.pushState({}, '', '/signup')
+
+    render(
+      <Router>
+        <App />
+      </Router>,
+    )
+
+    await waitFor(() => {
+      expect(screen.getByText(/Sign up/i)).toBeInTheDocument()
+    })
+  })
+})
